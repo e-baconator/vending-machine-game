9c3c9e7855721fe0ce485abcd0d126919d5bd29d
Ellie Bacon <bacon.e@northeastern.edu>
2025-07-13 10:46:10 +0900

    vending machine door animation
    door will open and close on "e"

--- Assets/Scripts/VMInteract.cs
+++ Assets/Scripts/VMInteract.cs
@@ -4,12 +4,33 @@ public class VMInteract : MonoBehaviour, IInteractable
 {
     public string InteractMessage => objectInteractMessage;
 
-    [SerializeField] string objectInteractMessage = "Press E to open";
+    string objectInteractMessage;
 
     [SerializeField] GameObject hinge;
 
+    bool isOpen;
+
     public void Interact()
     {
-        hinge.SetActive(true);
+        hinge.GetComponent<Animator>().enabled = true;
+        if (isOpen)
+        {
+            hinge.GetComponent<Animator>().Play("VMDoorClose");
+            objectInteractMessage = "Press E to open";
+            isOpen = false;
+        }
+        else
+        {
+            hinge.GetComponent<Animator>().Play("VMDoorOpen");
+            objectInteractMessage = "Press E to close";
+            isOpen = true;
+        }
+    }
+
+    public void Start()
+    {
+        isOpen = false;
+        objectInteractMessage = "Press E to open";
+        hinge.GetComponent<Animator>().enabled = false;
     }
 }
