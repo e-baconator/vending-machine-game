115e74c783b79cec36f6776115bcc95f048324ca
Ellie Bacon <bacon.e@northeastern.edu>
2025-07-18 09:05:49 +0900

    shelf progress

--- Assets/Scripts/SelectableShelf.cs
+++ Assets/Scripts/SelectableShelf.cs
@@ -10,27 +10,38 @@ public class SelectableShelf : MonoBehaviour, ISelectable
     public void SetChildPosition(Transform newChild)
     {
         newChild.SetParent(transform);
+        if (numBottles == 0)
+        {
+            newChild.localPosition = new Vector3(0, -0.002f, 0.028f);
+            newChild.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
     }
 
     public void Use()
     {
         if (numBottles < bottleCapacity && pickupHolder.childCount != 0)
         {
-            //pickedObject = (IPickupable)pickupHolder.GetChild(0);
             SetChildPosition(pickupHolder.GetChild(0));
             pickupHolder.GetComponentInParent<PickupController>().RemovePickup();
+            numBottles = numBottles + 1;
+        }
+        else if (numBottles == bottleCapacity && pickupHolder.childCount != 0)
+        {
+            // send a message to say the shelf is full
+        }
+        else if (numBottles > 0 && pickupHolder.childCount == 0)
+        {
+            Transform bottle = transform.GetChild(numBottles - 1);
+            bottle.parent = null;
+            //IPickupable pickupableBottle = (IPickupable)bottle;
+            bottle.GetComponent<PhysicsPickup>().Grab(pickupHolder.GetComponentInParent<PickupController>());
+            //pickupableBottle.Grab(pickupHolder.GetComponentInParent<PickupController>());
+            numBottles = numBottles - 1;
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         numBottles = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
