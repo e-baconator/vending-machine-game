b71a433abf775cdb1a8b2037bd704c7f3bf1939d
Ellie Bacon <bacon.e@northeastern.edu>
2025-07-24 14:44:12 +0900

    made vm status

---
+++ Assets/Scripts/InteractableShelves.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableShelves : MonoBehaviour
+{
+    private List<Transform> shelves = new List<Transform>();
+    public bool isFull;
+
+    void PopulateShelfList()
+    {
+        for (int row = 0; row < transform.childCount; row++)
+        {
+            for (int shelf = 0; shelf < transform.GetChild(row).childCount; shelf++)
+            {
+                shelves.Add(transform.GetChild(row).GetChild(shelf));
+            }
+        }
+    }
+
+    void Start()
+    {
+        isFull = false;
+        PopulateShelfList();
+    }
+
+    void Update()
+    {
+        foreach (Transform shelf in shelves)
+        {
+            if (shelf.childCount < 4)
+            {
+                return;
+            }
+        }
+
+        isFull = true;
+    }
+}
